160b4c01418c694dfabfd9eedc1441f4de4229a6
Amit Ashdot <amitashdot@gmail.com>
2022-11-16 09:53:10 +0200

    prepare for deploy

--- server.js
+++ server.js
@@ -35,15 +35,24 @@ app.use('/api/flight', require('./routes/api/flight'));
 app.use('/api/files', require('./routes/api/files'));
 app.use('/api/mail', require('./routes/api/mail'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => { res.send('  API Running  ') });
+// app.get('/', (req, res) => { res.send('  API Running  ') });
 
 app.listen(PORT, () => { console.log(`server started on port ${PORT}`) });
 
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 // mainFunction('./pdfFiles')
 // mainFunction();
